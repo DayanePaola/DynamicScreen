0ff0a00759498623c190930b94995b0499a2f99a
Dayane Ribeiro <dayanepaola@hotmail.com>
2020-12-03 18:01:36 -0300

    adicionado form para testes de inclusão dinâmica

---
+++ DynamicScreen/View/MainForm1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DynamicScreen
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(776, 426);
+            this.tabControl.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+    }
+}
+
---
+++ DynamicScreen/View/MainForm1.cs
@@ -0,0 +1,45 @@
+﻿using DynamicScreen.Data;
+using DynamicScreen.Data.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DynamicScreen
+{
+    public partial class MainForm : Form
+    {
+        public MainForm(string connString)
+        {
+            InitializeComponent();
+
+            using (var context = new Context(connString))
+            {
+                var query = context.Configurationn.Include(i => i.ConfigurationColumn).Include(i => i.ConfigurationRow).ToList();
+
+                query.ForEach(f =>
+                {
+                    var tab = AddTab(f.Name, f.Title);
+                    AddComponents(f.ConfigurationColumn.ToList(), tab);
+                });
+            }
+        }
+
+        private TabPage AddTab(string name, string text)
+        {
+            TabPage tbp = new TabPage();
+            tbp.Name = name;
+            tbp.Text = text;
+            tabControl.TabPages.Add(tbp);
+            return tbp;
+        }
+
+        private void AddComponents(List<ConfigurationColumnModel> configurationColumns, TabPage tab)
+        {
+            configurationColumns.ForEach(f=>
+            {
+                f.Component == 
+            });
+        }
+    }
+}
