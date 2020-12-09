4fdc6bdfcba4e79c8ea85f67b0c96498526f822a
Dayane Ribeiro <dayanepaola@hotmail.com>
2020-12-09 20:27:46 -0300

    add property to check if field is readonly

--- DynamicScreen/Dto/ConfigurationColumnDto.cs
+++ DynamicScreen/Dto/ConfigurationColumnDto.cs
@@ -8,6 +8,7 @@ namespace DynamicScreen.Dto
         public int Id { get; set; }
         public string Title { get; set; }
         public string Name { get; set; }
+        public bool ReadOnly { get; set; }
         public List<ValueDto> EnableValues { get; set; }
     }
 }
